76c995d95b1aa3b8d0995224a315f0a346229270
KristijanV <kvuje60@gmail.com>
2023-12-21 13:38:35 +0100

    123

--- src/app/Components/MessageContext.js
+++ src/app/Components/MessageContext.js
@@ -1,26 +1,20 @@
 // MessageContext.js
-import { createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 
-const MessageContext = createContext();
+export const MessageContext = createContext();
 
 const MessageContextProvider = ({ children }) => {
-  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
   const addMessage = (newMessage) => {
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { message: newMessage.message, sender: newMessage.sender },
-    ]);
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
   };
 
   return (
-    <MessageContext.Provider
-      value={{ message, setMessage, messages, setMessages, addMessage }}
-    >
+    <MessageContext.Provider value={{ messages, addMessage }}>
       {children}
     </MessageContext.Provider>
   );
 };
 
-export { MessageContext, MessageContextProvider };
+export default MessageContextProvider;
