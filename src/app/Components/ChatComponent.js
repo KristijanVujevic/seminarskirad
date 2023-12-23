3792c4eedbf4faec18ce2c11d6978eb94c566e50
KristijanV <kvuje60@gmail.com>
2023-12-23 13:25:26 +0100

    123

--- src/app/Components/ChatComponent.js
+++ src/app/Components/ChatComponent.js
@@ -1,13 +1,16 @@
-// ChatComponent.js
 import React, { useContext, useEffect, useState } from "react";
 import { MessageContext } from "./MessageContext";
 import { useScaledrone } from "./ScaledroneContext";
+import Link from "next/link";
 import { auth, firestore } from "@/app/Components/firebase";
 import UserInput from "./UserInput";
 import { Button, Container, Row, Col } from "react-bootstrap";
+import { Icon } from "@iconify/react";
+import { useRouter } from "next/navigation";
 import styles from "@/app/page.module.css";
 
 const ChatComponent = () => {
+  const router = useRouter();
   const messageContext = useContext(MessageContext);
   const { drone } = useScaledrone();
 
@@ -24,80 +27,76 @@ const ChatComponent = () => {
     };
   }, []);
 
-  React.useEffect(() => {
-    let room;
-
-    const initRoom = async () => {
-      if (!room) {
-        room = drone.subscribe("observable-my-room");
-
-        room.on("open", (error) => {
-          if (error) {
-            console.error(error);
-          } else {
-            console.log(`Connected to room`);
-          }
-        });
-
-        room.on("message", (messageData) => {
-          console.log("Received message:", messageData);
-          messageContext.addMessage({
-            id: messageData.data.id,
-            message: messageData.data.message,
-            sender: messageData.data.sender,
-          });
-        });
-
-        drone.on("error", (error) => console.error(error));
-      }
-    };
-
-    const fetchUserData = async () => {
-      try {
-        const doc = await firestore.collection("users").doc(user.uid).get();
-        if (doc.exists) {
-          setUserData(doc.data());
-        } else {
-          console.error("User document does not exist");
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error.message);
-      }
-    };
-
-    if (user && drone) {
-      initRoom();
-      fetchUserData();
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      router.push("/signin");
+    } catch (error) {
+      console.error("Error signing out:", error.message);
     }
+  };
 
-    return () => {
-      if (room) {
-        room.unsubscribe();
-      }
+  useEffect(() => {
+    if (user && drone) {
+      const room = drone.subscribe("observable-my-room");
 
-      if (drone && drone.client) {
-        drone.client.close();
-      }
-    };
-  }, []);
+      room.on("open", (error) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(`Connected to room`);
+        }
+      });
+
+      room.on("message", (messageData) => {
+        console.log("Received message:", messageData);
+        messageContext.setMessage(messageData.data.message);
+      });
+
+      drone.on("error", (error) => console.error(error));
+
+      const fetchUserData = async () => {
+        try {
+          const doc = await firestore.collection("users").doc(user.uid).get();
+          if (doc.exists) {
+            setUserData(doc.data());
+          } else {
+            console.error("User document does not exist");
+          }
+        } catch (error) {
+          console.error("Error fetching user data:", error.message);
+        }
+      };
+
+      fetchUserData();
+
+      return () => {
+        if (drone.client) {
+          drone.client.close();
+        }
+      };
+    }
+  }, [user, drone, messageContext]);
 
   const renderMessage = (messageData) => {
-    console.log("Rendering a message!");
-    const isMyMessage = messageData.sender === user?.uid;
-    const senderUsername = isMyMessage ? "You" : "Other User";
+    const isMyMessage = messageData.message.sender === user?.uid;
+    const senderUsername =
+      messageData.message.sender === user?.uid
+        ? "You"
+        : messageData.senderUsername;
 
     return (
       <div
-        key={messageData.id}
+        key={messageData.score}
         className={
           isMyMessage
             ? `${styles.message} ${styles.myMessage}`
             : `${styles.message} ${styles.otherUserMessage}`
         }
       >
-        {messageData && messageData.message ? (
+        {messageData && messageData.message.message ? (
           <div>
-            <p>{messageData.message}</p>
+            <p>{messageData.message.message}</p>
             <p>Sent by: {senderUsername}</p>
           </div>
         ) : null}
@@ -123,10 +122,20 @@ const ChatComponent = () => {
           <Col>
             <Col className={styles.messagesContainer}>
               {messageContext.messages.map((messageData) =>
-                renderMessage(messageData)
+                renderMessage({
+                  ...messageData,
+                  senderUsername:
+                    userData?.uid === messageData.message.sender
+                      ? "You"
+                      : userData?.username,
+                })
               )}
             </Col>
-            <Button variant="primary" className={styles.card}>
+            <Button
+              variant="primary"
+              className={styles.card}
+              onClick={handleLogout}
+            >
               Logout
             </Button>
           </Col>
@@ -137,13 +146,13 @@ const ChatComponent = () => {
       ) : (
         <Row className={styles.grid}>
           <Col>
-            <p>Sign in!</p>
+            <Link href={"/signin"}>Sign in!</Link>
           </Col>
           <Col>
             <br />
           </Col>
           <Col>
-            <p>Create an account!</p>
+            <Link href={"/signup"}>Create an account!</Link>
           </Col>
         </Row>
       )}
